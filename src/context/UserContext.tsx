40db775abf1f8caae5d6fc99fa3a39d52eaa90ad
ssekabira robert sims <ssekabirarobert037@gmail.com>
2025-08-14 12:40:55 +0300

    Comment out unused User interface definition for clarity

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-interface User {
-  login_id: string;
-  date: string;
-  request_id: string;
-  error: unknown;
-  request_status: number;
-  data: { 
-    username: string;
-    email: string;
-    token: string;
-    message: string;
-    status: string;
-    signedUp: boolean;
-  };
-}
+// interface User {
+//   login_id: string;
+//   date: string;
+//   request_id: string;
+//   error: unknown;
+//   request_status: number;
+//   data: { 
+//     username: string;
+//     email: string;
+//     token: string;
+//     message: string;
+//     status: string;
+//     signedUp: boolean;
+//   };
+// }
 
 const LoggedInUserInformationObjectContent: any = React.createContext(
   window.localStorage.getItem("user")
